b26073c7b015b2c269f81496f7a83c2578619899
Vyacheslav Kalaushin <kalaushin2000@yandex.ru>
2022-04-05 13:15:31 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,60 @@
+/**F(x)=x^(32)⊕a*x(t)⊕1, где t-номер студента в списке\n\n
+ * f(x)=x^(16)+x^(12)+x^(3)+x+1 => 0001000000001011 => 0x100B
+ */
+#include "stdio.h"
+
+void rec(int n, unsigned short *seq) {
+    unsigned short tmp;
+    for (int i = 0; i < n; i++) {
+        tmp = seq[10] << 1;
+        if (tmp & 0x8000) {
+            tmp ^= 0x100B;
+        }
+        tmp ^= seq[0];
+        for (int j = 0; j < 31; j++) {
+            seq[j] = seq[j + 1];
+        }
+        seq[31] = tmp;
+    }
+}
+
+int rec1(int n, unsigned short *seq, int currentIndex) {
+    unsigned short tmp;
+    for (int i = 0; i < n; i++) {
+        tmp = seq[(10 + currentIndex) & 31] << 1;
+        if (tmp & 0x8000) {
+            tmp ^= 0x100B;
+        }
+        tmp ^= seq[currentIndex & 31];
+        seq[(currentIndex & 31)] = tmp;
+        currentIndex++;
+    }
+    return currentIndex;
+}
+
+int main() {
+
+    int t = 10;
+    unsigned short seq[32] = {0};
+    for (int i = 0; i < 32; i++) {
+        seq[i] = t + i;
+    }
+
+    printf("\n<<<<<Method 1>>>>>\n");
+    rec(63, seq);
+    for (int i = 64; i < 96; i++) {
+        rec(1, seq);
+        printf("Element %d: %hu\n", i, seq[31]);
+    }
+
+    printf("\n<<<<<Method 2>>>>>\n");
+    for (int i = 0; i < 32; i++) {
+        seq[i] = t + i;
+    }
+    int currentIndex = 0;
+    currentIndex = rec1(63, seq, currentIndex);
+    for (int i = 64; i < 96; i++) {
+        currentIndex = rec1(1, seq, currentIndex);
+        printf("Element %d: %hu\n", i, seq[(currentIndex - 1) & 31]);
+    }
+}
